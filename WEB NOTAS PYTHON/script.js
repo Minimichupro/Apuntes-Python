7edefa7f31503dcb411645d65dfe95d215285263
Minimichupro <anaonix1970@gmail.com>
2026-05-07 16:50:59 +0200

    Delete WEB NOTAS PYTHON directory

--- WEB NOTAS PYTHON/script.js
+++
@@ -1,8 +0,0 @@
-const botones = document.querySelectorAll(".boton-desplegable");
-
-botones.forEach(function (boton) {
-  boton.addEventListener("click", function () {
-    const contenido = boton.nextElementSibling;
-    contenido.classList.toggle("abierto");
-  });
-});
